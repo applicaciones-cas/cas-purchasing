996364b83f9f1906c7d88b361906ee42dad0b049
Jeitee <93184955+jeitee@users.noreply.github.com>
2026-06-16 11:55:44 +0800

    Update PurchaseOrder.java

--- src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
+++ src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
@@ -4393,7 +4393,7 @@ public class PurchaseOrder extends Transaction {
             
             lsFilter.add("b.sCompnyID = " + SQLUtil.toSQL(Master().getCompanyID()) 
                     + " AND b.sIndstCdx = " +  SQLUtil.toSQL(Master().getIndustryID())
-                    + " b.sTransNox LIKE " +  SQLUtil.toSQL(poGRider.getBranchCode()+ "%") );
+                    + " AND b.sTransNox LIKE " +  SQLUtil.toSQL(poGRider.getBranchCode()+ "%") );
             
             if (psTranStat.length() > 1) {
                 for (int lnCtr = 0; lnCtr <= psTranStat.length() - 1; lnCtr++) {
