8dc2914235e733ae4e182ef0998b82a626e7c1dc
Jeitee <93184955+jeitee@users.noreply.github.com>
2026-06-16 09:24:14 +0800

    06162026
    and where clause by sbranchcd

--- src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
+++ src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrder.java
@@ -4257,7 +4257,8 @@ public class PurchaseOrder extends Transaction {
             }
             
             lsFilter.add("a.sCompnyID = " + SQLUtil.toSQL(Master().getCompanyID()) 
-                    + " AND a.sIndstCdx = " +  SQLUtil.toSQL(Master().getIndustryID()));
+                    + " AND a.sIndstCdx = " +  SQLUtil.toSQL(Master().getIndustryID())
+                    + " AND a.sTransNox LIKE " + SQLUtil.toSQL(poGRider.getBranchCode()) + "%");
             
             if (psTranStat.length() > 1) {
                 for (int lnCtr = 0; lnCtr <= psTranStat.length() - 1; lnCtr++) {
@@ -4391,7 +4392,8 @@ public class PurchaseOrder extends Transaction {
             }
             
             lsFilter.add("b.sCompnyID = " + SQLUtil.toSQL(Master().getCompanyID()) 
-                    + " AND b.sIndstCdx = " +  SQLUtil.toSQL(Master().getIndustryID()));
+                    + " AND b.sIndstCdx = " +  SQLUtil.toSQL(Master().getIndustryID())
+                    + " b.sTransNox LIKE " +  SQLUtil.toSQL(poGRider.getBranchCode()) + "%");
             
             if (psTranStat.length() > 1) {
                 for (int lnCtr = 0; lnCtr <= psTranStat.length() - 1; lnCtr++) {
