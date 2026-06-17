4f095f5c8dec35b934a2da512cc4fd195fc61932
Arsiela <arsiela68@gmail.com>
2026-06-17 17:09:36 +0800

    06172026 - Update Purchase Order Return
    - Added functionality for paid transaction method

--- src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrderReturn.java
+++ src/main/java/org/guanzon/cas/purchasing/controller/PurchaseOrderReturn.java
@@ -716,7 +716,139 @@ public class PurchaseOrderReturn extends Transaction{
 
         return poJSON;
     }
+    
+    /**
+     * Paid the transaction
+     * @param remarks
+     * @return JSON
+     * @throws ParseException
+     * @throws SQLException
+     * @throws GuanzonException
+     * @throws CloneNotSupportedException 
+     */
+    public JSONObject PaidTransaction(String remarks)
+            throws ParseException,
+            SQLException,
+            GuanzonException,
+            CloneNotSupportedException {
+        poJSON = new JSONObject();
 
+        String lsStatus = PurchaseOrderReturnStatus.PAID;
+        boolean lbPaid = true;
+
+        if (getEditMode() != EditMode.READY) {
+            poJSON.put("result", "error");
+            poJSON.put("message", "No transacton was loaded.");
+            return poJSON;
+        }
+
+        MatrixAuthChecker check = null; 
+        
+        if(!pbWthParent){
+            //validator
+            poJSON = isEntryOkay(lsStatus);
+            if (!"success".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            //get the matrix return from isEntryOkey
+            JSONArray loMatrix = (JSONArray) poJSON.get("matrix");
+
+            //Check if there is a authorization request
+            if(loMatrix != null){
+                //initialized MatrixAuthChecker object
+                check = new MatrixAuthChecker(poGRider, SOURCE_CODE, Master().getTransactionNo());
+                //load the current autorization matrix request
+                poJSON = check.loadAuth();
+
+                //check if loading is okey
+                if (!"success".equals((String) poJSON.get("result"))) {
+                    return poJSON;
+                }
+
+                //check if authorization request is already approved by all authorizing personnel
+                if(!check.isAuthOkay()){
+                    //check if authorization request allows system approval
+                    if(!check.isAllowSys()){
+                        //extract the JSONObject from JSONArray
+                        JSONObject loJson = (JSONObject)loMatrix.get(0);
+
+                        //check if current user is authorized to approved this transaction
+                        poJSON = check.authTrans((String) loJson.get("sAuthType"), poGRider.getUserID());
+                        
+                        //If not authorized/request system approval
+                        if(!"success".equalsIgnoreCase((String)poJSON.get("result"))){
+                            poJSON = ShowDialogFX.getUserApproval(poGRider);
+                            if("error".equals((String)poJSON.get("result"))){
+                                return poJSON;
+                            }
+
+                            //check if approving officer is authorized
+                            String lsUserIDxx = poJSON.get("sUserIDxx").toString();
+                            //check if current user is authorized to approved this transaction
+                            poJSON = check.authTrans((String) loJson.get("sAuthType"), poGRider.getUserID());
+                            //user is not authorized
+                            if(!"success".equalsIgnoreCase((String)poJSON.get("result"))){
+                                return poJSON;
+                            }
+                            
+                            setApproving(lsUserIDxx);
+                        }
+                    }
+
+                    //check if authorization request is already approved by all authorizing personnel
+                    if(!check.isAuthOkay()){
+                        poGRider.beginTrans("UPDATE STATUS", "PaidTransaction", SOURCE_CODE, Master().getTransactionNo());
+
+                        lsStatus = Character.toString((char)(64 + Integer.parseInt(lsStatus)));
+                        poJSON = statusChange(poMaster.getTable(), (String) poMaster.getValue("sTransNox"), remarks, lsStatus, !lbPaid, true);
+                        if (!"success".equals((String) poJSON.get("result"))) {
+                            poGRider.rollbackTrans();
+                            return poJSON;
+                        }
+
+                        poGRider.commitTrans();
+                        
+                        poJSON.put("result", "matrix");
+                        return poJSON;
+                    }
+                }
+            }
+            //there are no authorization event request
+            else{
+                //Replaced script above by calling of method Arsiela 10-15-2025 09:25:01
+                poJSON = seekApproval();
+                if("error".equalsIgnoreCase((String)poJSON.get("result"))){
+                    return poJSON;
+                }
+            }
+        }
+
+        //poGRider.beginTrans("UPDATE STATUS", "PaidTransaction", SOURCE_CODE, Master().getTransactionNo());
+        
+        //change status
+        poJSON = statusChange(poMaster.getTable(), (String) poMaster.getValue("sTransNox"), remarks, lsStatus, !lbPaid, pbWthParent);
+        if (!"success".equals((String) poJSON.get("result"))) {
+            return poJSON;
+        }
+
+        if(check != null){
+            check.postAuth();
+        }
+        
+        //poGRider.commitTrans();
+        
+        poJSON = new JSONObject();
+        poJSON.put("result", "success");
+        if (lbPaid) {
+            poJSON.put("message", "Transaction paid successfully.");
+        } else {
+            poJSON.put("message", "Transaction paid request submitted successfully.");
+        }
+
+        return poJSON;
+    }
+    
     public JSONObject CancelTransaction(String remarks)
             throws ParseException,
             SQLException,
